02d7887cb4c7d8e7cbdf5d9bbe07f8777d47be8d
ckbakunas <colinbakunas@gmail.com>
2025-03-09 22:30:52 -0400

    Task 2 - Employee Cards Dynamic Addition

---
+++ cc_13.js
@@ -0,0 +1,33 @@
+// Task 2: Adding Employee Cards Dynamically
+
+
+function addEmployeeCard(name, position) { // employee card container
+
+
+    const card = document.createElement('div');
+    card.setAttribute('class', 'employee-card'); // class for style
+
+    const heading = document.createElement('h3'); // heading
+    heading.textContent = name;
+
+    const paragraph = document.createElement('h3'); // position is a paragragh
+    paragraph.textContent = position;
+
+    const removeButton = document.createElement('button'); // remove button
+    removeButton.textContent = 'Remove';
+
+    const editButton = document.createElement('button'); // edit button - task 5
+    editButton.textContent = 'Edit';
+
+    // appending elements to the card
+    card.appendChild(heading);
+    card.appendChild(paragraph);
+    card.appendChild(editButton);
+    card.appendChild(removeButton);
+    card.appendChild(editButton);
+
+    // appending card to employee container
+    document.getElementById('employeeContainer').appendChild(card);
+
+    // remove functionality
+    removeButton.addEventListener('click', function(event) {
